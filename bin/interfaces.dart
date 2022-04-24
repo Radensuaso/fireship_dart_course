9db6b3fbe50800896c017499722e9666eba72634
André Sousa <66281967+Radensuaso@users.noreply.github.com>
2022-04-24 17:08:07 +0100

    interfaces done

---
+++ bin/interfaces.dart
@@ -0,0 +1,27 @@
+void main(List<String> args) {
+  var e = Elephant('Bob');
+
+  // works everywhere
+  e.sayHi();
+
+  // only works in this file
+  e._saySecret();
+}
+
+// abstract --> none instantiable classes like interfaces in typescript
+class Elephant {
+  // Public interface
+  final String name;
+
+  // In the interface, but visible only in this library. (private)
+  final int _id = 23;
+
+  // Not in the interface, since this is a constructor.
+  Elephant(this.name);
+
+  // Public method.
+  sayHi() => 'My name is $name.';
+
+  // Private method.
+  _saySecret() => 'My ID is $_id.';
+}
